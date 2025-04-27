1b0b824eae7f60335f22a1c34a0fc8ba6ebdc956
Anil <khalifaanil84@gmail.com>
2025-04-27 15:07:53 +0545

    update entry point with the proper documentation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+/**
+ * @file index.ts
+ * @description This file serves as the entry point for the compiler. It initializes the tokenizer and parser, and provides a function to compile code.
+ * @license MIT
+ * @author Anil Bhandari
+ * @version 1.0.0
+ * @date 2023-10-01
+ * @includes
+ * - Tokenizer: A class that tokenizes the input code.
+ * - Parsing: A class that parses the tokens into an Abstract Syntax Tree (AST).
+ * - eval_program: A function that evaluates the AST and returns the result.
+ * - resetGlobalEnvironment: A function that resets the global environment for the interpreter.
+ * - CustomError: A custom error class for handling errors in the compiler.
+ * @exports
+ * - compile: A function that takes a string of code, tokenizes it, parses it, and evaluates it.
+ * - resetCompilerState: A function that resets the state of the compiler.
+ * - Tokenizer: The Tokenizer class.
+ * - Parsing: The Parsing class.
+ */
+
 import { Tokenizer } from "./lexer/Tokenizer";
 import { CustomError } from "./parser/errors/CustomErrors";
 import { Parsing } from "./parser/parsing/_index";
@@ -8,11 +28,26 @@ import { RuntimeVal } from "./interpreter/semanticAnalyzer/values";
 const tokenizer = new Tokenizer();
 const parsing = new Parsing();
 
+/**
+ * @function resetCompilerState
+ * @description Resets the state of the compiler by reinitializing the tokenizer and parser, and resetting the global environment.
+ * @returns void
+ */
 export function resetCompilerState() {
   parsing.reset();
   resetGlobalEnvironment();
 }
 
+/**
+ * 
+ * @param code The code to be compiled.
+ * @description This function takes a string of code, tokenizes it, parses it, and evaluates it.
+ * It returns the result of the evaluation and any outputs generated during the process.
+ * @throws {Error} Throws an error if the code cannot be compiled.
+ * @throws {CustomError} Throws a custom error if there is an issue with the code.
+ * @throws {UnexpectedError} Throws an unexpected error if there is an issue with the code.
+ * @returns { results: RuntimeVal; outputs: string[] } The results of the evaluation and any outputs generated.
+ */
 export function compile(code: string): { results: RuntimeVal; outputs: string[] } {
   tokenizer.initTokenizer(code);
 
@@ -25,9 +60,9 @@ export function compile(code: string): { results: RuntimeVal; outputs: string[] 
         tokens.push(token);
       }
     }
-    
+
     parsing.initializeParsing(tokens);
-    
+
     const ast = parsing.producedAST();
     return eval_program(ast);
 
